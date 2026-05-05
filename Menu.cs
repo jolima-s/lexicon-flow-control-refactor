7cc502b0db9ec341147258446cdaa015450b636b
Johanna Sewring <j_sewring@hotmail.com>
2026-05-05 13:46:39 +0200

    Moved menu functionality to separate file.

---
+++ Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ovn2_FlowControl
+{
+    internal class Menu
+    {
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("Välkommen till huvudmenyn.");
+            Console.WriteLine("Skriv en siffra för att välja funktion.");
+            Console.WriteLine("0 = Avsluta");
+            Console.WriteLine("1 = Ungdom eller pensionär");
+            Console.WriteLine("2 = Pris för sällskap");
+            Console.WriteLine("3 = Upprepa tio gånger");
+            Console.WriteLine("4 = Det tredje ordet");
+            Console.Write("Ditt val: ");
+        }
+    }
+}
